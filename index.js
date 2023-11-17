fc07811c1c474ffa8ea781351324f42c70426437
Rahul Bhatt <72143946+rahul-bhatt43@users.noreply.github.com>
2023-11-17 19:29:11 +0530

    added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import morgan from 'morgan'
 const app = express();
 
 
 
 
 dotenv.config();
-app.use(morgan('default'));
+
 app.use(express.json())
 app.use(cors());
 
